c575629d7f9b94ef2b9934438a0231aa9c2701ef
tsopsericado <ricardotsopse@gmail.com>
2023-07-14 04:05:50 +0100

    some dependencies installed succesfully

---
+++ chat_appli/src/App.js
@@ -0,0 +1,8 @@
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+
+function App() {
+  return <ChatEngine height="100vh" projectID="" userName="" userSecret="" />;
+}
+
+export default App;
