1f3181285a512a2750b7448145cd908b3c518fa4
tsopsericado <ricardotsopse@gmail.com>
2023-07-16 04:13:08 +0100

    some functionality added(new users functionality integration

--- chat_appli/src/App.js
+++ chat_appli/src/App.js
@@ -2,7 +2,14 @@ import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 
 function App() {
-  return <ChatEngine height="100vh" projectID="" userName="" userSecret="" />;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="8f75e0ff-1073-4386-a304-ba5b358418ae"
+      userName="ricardo"
+      userSecret="Maman123"
+    />
+  );
 }
 
 export default App;
